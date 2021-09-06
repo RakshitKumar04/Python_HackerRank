1d4f990c82eef3a967f127ecdb79b29bb1a3bf60
RakshitKumar-04 <kumarrakshit0402@gmail.com>
2021-09-06 10:21:09 +0530

    Committed: Collection Counter

---
+++ Collection_Counter.py
@@ -0,0 +1,11 @@
+numShoes = int(input())
+shoes = collections.Counter(map(int, input().split()))
+numCust = int(input())
+income = 0
+for i in range(numCust):
+    		size, price = map(int, input().split())
+    		if shoes[size]: 
+        		income += price
+        		shoes[size] -= 1
+
+print(income)
